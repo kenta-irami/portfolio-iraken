1a02dfbc44dcc6735c4d7e62c11a6524f29c2816
iraken <iramina_kenta@yahoo.co.jp>
2025-12-01 20:47:33 +0900

    Fix MDX component registration and update project structure

--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { MDXRemote } from 'next-mdx-remote/rsc'
 import { highlight } from 'sugar-high'
 import React from 'react'
+import { ProjectTechBadges } from 'app/components/project-tech-badges'
 
 function Table({ data }) {
   let headers = data.headers.map((header, index) => (
@@ -97,6 +98,7 @@ let components = {
   a: CustomLink,
   code: Code,
   Table,
+  ProjectTechBadges,
 }
 
 export function CustomMDX(props) {
